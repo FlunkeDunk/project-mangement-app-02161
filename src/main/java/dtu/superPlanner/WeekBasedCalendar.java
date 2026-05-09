9ce4328da1cc9340b9ec8631305b856ceda4248c
Benjamin Ewe <184654065+BenjaminEwe@users.noreply.github.com>
2026-05-09 15:43:51 +0200

    NormalizeInput private method
    Created a normalizeInput method that reduces redundancy of setWeek and constructor.

--- src/main/java/dtu/superPlanner/WeekBasedCalendar.java
+++ src/main/java/dtu/superPlanner/WeekBasedCalendar.java
@@ -2,7 +2,6 @@ package dtu.superPlanner;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.IsoFields;
 import java.time.temporal.WeekFields;
@@ -12,20 +11,12 @@ public class WeekBasedCalendar {
     private int year;
 
     /**
-     * @author Emanuel
+     * @author BenjaminEwe
      */
     public WeekBasedCalendar(int week, int year) {
-        if (week == 0) {
-            throw new IllegalArgumentException(String.format("DateError: Invalid week: %d", week));
-        } else if (week < 0) {
-            // Since no week 0 exists, we take negative weeks to mean weeks before first
-            // week of a given year
-            week++;
-        }
-
-        LocalDate date = LocalDate.of(year, 1, 4).plusWeeks(week - 1);
-        this.week = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
-        this.year = date.getYear();
+        WeekAndYear normalized = normalizeInput(week, year);
+        this.week = normalized.week();
+        this.year = normalized.year();
     }
 
     /**
@@ -52,32 +43,14 @@ public class WeekBasedCalendar {
      * @author BenjaminEwe
      */
     public void setWeek(int week) {
-        assert true;
-        if (week == 0) {
-            throw new IllegalArgumentException(String.format("DateError: Invalid week: %d", week));
-        } else if (week < 0) {
-            // Since no week 0 exists, we take negative weeks to mean weeks before first
-            // week of a given year
-            week++;
-        }
-        int priorYear = this.year;
-
-        LocalDate date = LocalDate.of(year, 1, 4).plusWeeks(week - 1);
-        this.week = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
-        this.year = date.getYear();
-
-        // The new week number must at most be the last week of the year we are in and at least 1 to be a valid week number
-        assert(this.week <= LocalDate.of(this.year, 12, 28).get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
-                && this.week > 0);
-        // The new week and year must be exactly 'week' weeks after
-        assert(ChronoUnit.WEEKS.between(
-                LocalDate.of(priorYear, 1, 4),
-                LocalDate.now()
-                        .withYear(this.year)
-                        .with(WeekFields.ISO.weekOfWeekBasedYear(), this.week)
-                        .with(WeekFields.ISO.dayOfWeek(), 1)) == week);
+        WeekAndYear normalized = normalizeInput(week, this.year);
+        this.week = normalized.week();
+        setYear(normalized.year());
     }
 
+    /**
+     * @author Mikkel
+     */
     public LocalDate toLocalDate() {
         WeekFields weekFields = WeekFields.ISO;
         return LocalDate
@@ -94,6 +67,41 @@ public class WeekBasedCalendar {
         return year < other.year || (year == other.year && week < other.week);
     }
 
+    /**
+     * @author BenjaminEwe
+     */
+    private WeekAndYear normalizeInput(int week, int year) {
+        assert true;
+        if (week == 0) {
+            throw new IllegalArgumentException(String.format("DateError: Invalid week: %d", week));
+        } else if (week < 0) {
+            // Since no week 0 exists, we take negative weeks to mean weeks before first
+            // week of a given year
+            week++;
+        }
+        LocalDate date = LocalDate.of(year, 1, 4).plusWeeks(week - 1);
+        int newWeek = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+        int newYear = date.getYear();
+
+        // The new week number must at most be the last week of the year we are in and at least 1 to be a valid week number
+        assert(newWeek <= LocalDate.of(newYear, 12, 28).get(IsoFields.WEEK_OF_WEEK_BASED_YEAR)
+                && newWeek > 0);
+        // The new date is 'week' weeks after the input years first week
+        assert(ChronoUnit.WEEKS.between(
+                LocalDate.of(year, 1, 4).with(WeekFields.ISO.dayOfWeek(), 1),
+                LocalDate.of(newYear, 1, 4)
+                        .withYear(newYear)
+                        .with(WeekFields.ISO.weekOfWeekBasedYear(), newWeek)
+                        .with(WeekFields.ISO.dayOfWeek(), 1)) == week - 1);
+
+        return new WeekAndYear(newWeek, newYear);
+    }
+
+    private record WeekAndYear(int week, int year) {}
+
+    /**
+     * @author Mikkel
+     */
     @Override
     public String toString() {
         return year + "-W" + week;
