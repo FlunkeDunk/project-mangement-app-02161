780168ca34ca6124f38c95d99234e68979847da4
Ebb <ebbemandsen@gmail.com>
2026-05-10 16:15:17 +0200

    AddEmployeeToActivityTest unnecessary code

--- src/test/java/dtu/superPlanner/AddEmployeeToActivityTest.java
+++ src/test/java/dtu/superPlanner/AddEmployeeToActivityTest.java
@@ -5,22 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AddEmployeeToActivityTest {
     private ProjectManagementApp app = new ProjectManagementApp(new FileEmployeeRepository(), new MemoryProjectRepository());
-    private Project testProject1 = app.createProject("Test Project1");
-    private Project testProject2 = app.createProject("Test Project2");
-    private Project testProject3 = app.createProject("Test Project3");
-    private Activity testActivity1;
-    private Activity testActivity2;
-    private Activity testActivity3;
-
-    String errMessage = "";
+    private Project project;
+    private Activity activity;
 
     public AddEmployeeToActivityTest() throws IllegalAccessException {
         WeekBasedCalendar startWeek = new WeekBasedCalendar(1, 2049);
         WeekBasedCalendar endWeek = new WeekBasedCalendar(2, 2049);
         TimeFrame timeFrame = new TimeFrame(startWeek, endWeek);
-        this.testActivity1 = app.createActivity(testProject1.getId(), "Test Activity1", timeFrame);
-        this.testActivity2 = app.createActivity(testProject2.getId(), "Test Activity2", timeFrame);
-        this.testActivity3 = app.createActivity(testProject3.getId(), "Test Activity3", timeFrame);
+        project = app.createProject("Test Project");
+        activity = app.createActivity(project.getId(), "Test Activity", timeFrame);
 
         app.createEmployee("adut");
         app.createEmployee("mtie");
@@ -29,11 +22,12 @@ public class AddEmployeeToActivityTest {
     }
 
     @Test
-    public void testProjectLeaderOnly() throws IllegalAccessException {
+    public void testNotProjectLeader() throws IllegalAccessException {
         // Set A
-        app.setProjectLeader(testProject1.getId(), "mtie");
+        app.setProjectLeader(project.getId(), "mtie");
+        String errMessage = "";
         try {
-            app.addEmployeeToActivity(testProject1.getId(), testActivity1.getId(), "jdat");
+            app.addEmployeeToActivity(project.getId(), activity.getId(), "jdat");
         } catch (Exception e) {
             errMessage = e.getMessage();
         }
@@ -44,10 +38,10 @@ public class AddEmployeeToActivityTest {
     @Test
     public void testEmployeeAlreadyAssigned() throws IllegalAccessException {
         // Set B
-        app.addEmployeeToActivity(testProject2.getId(), testActivity2.getId(), "jdat");
-
+        app.addEmployeeToActivity(project.getId(), activity.getId(), "jdat");
+        String errMessage = "";
         try {
-            app.addEmployeeToActivity(testProject2.getId(), testActivity2.getId(), "jdat");
+            app.addEmployeeToActivity(project.getId(), activity.getId(), "jdat");
         } catch (Exception e) {
             errMessage = e.getMessage();
         }
@@ -58,8 +52,8 @@ public class AddEmployeeToActivityTest {
     @Test
     public void testEmployeeAssigned() throws IllegalAccessException {
         // Set C
-        app.addEmployeeToActivity(testProject3.getId(), testActivity3.getId(), "jdat");
+        app.addEmployeeToActivity(project.getId(), activity.getId(), "jdat");
 
-        assertTrue(testActivity3.getEmployees().contains("jdat"));
+        assertTrue(activity.getEmployees().contains("jdat"));
     }
 }
